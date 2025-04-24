f7889f218ed93932ad24a0077db3d4803f867246
JustinTLong16 <justinlong0815@gmail.com>
2025-04-24 09:56:02 -0500

    added PWManager and Database file. Made class for it and it takes the inputs required and appends them to a list, as well as started the opening of the program

---
+++ PWManager.py
@@ -0,0 +1,35 @@
+#import section
+import pandas as pd
+
+#functions/global variables/classes
+#its recomended to use a class
+account = []
+n= input("Are you a new user? (Y/N) ")
+class pwmanager:
+    def __init__(self): #this makes it so every time we make a new account object it will ask for name, user, pass, and category.
+        #TODO Make it so the user can input something to create a randomly generated password, also check for the password criteria as described in the README.md
+        self.name = input("What is your name? ")
+        self.user = input("what is your username? ")
+        self.password = input("What is your password? ")
+        self.category = input("What is your category? ")
+    def AppendEntry(self): #this appends it to the account list
+        account.append(self.name)
+        account.append(self.user)
+        account.append(self.password)
+        account.append(self.category)
+    def SaveTheData(): #This will use the pandas module to save the data
+        pass
+    def GetAccount(): #This will use the pandas module to grab the data
+        pass
+
+
+#mainloop section
+#this will be the section where we ask the user if he/she/they wants to make an account or get an existing one.
+if n == "y":
+    pass
+elif n == "n":
+    a1=pwmanager()
+    pwmanager.AppendEntry(a1)
+    print(account)
+    #pwmanager.SaveTheData()
+        
